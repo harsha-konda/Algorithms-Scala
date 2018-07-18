3027abb5c9adb65ef754ab3d6beed69e8ae9d856
harsha konda <harsha@harshas-MacBook-Pro-2.local>
2018-07-17 23:03:23 -0700

    n queens

---
+++ src/main/scala/search/nqueens/NaiveQueens.scala
@@ -0,0 +1,26 @@
+package search.nqueens
+
+object NaiveQueens {
+  def queen(n:Int) : Set[List[Int]] = {
+    def placeQueens(k: Int): Set[List[Int]] = {
+      if (k==0) Set(List())
+      else {
+        for {
+          queens <- placeQueens(k-1)
+          col <- 0 until n
+          if isSafe(col, queens)
+        } yield col :: queens
+      }
+    }
+
+    def isSafe(col : Int,l : List[Int]) : Boolean ={
+      val row = l.length
+      val positions = (0 until row) zip l
+      positions forall {
+        case (r,c) =>  c!=col && row-r != Math.abs(col-c)  // slope != 1
+      }
+    }
+
+    placeQueens(n)
+  }
+}
