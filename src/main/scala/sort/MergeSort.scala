bd5f5e990e7fc6a7768f00176cb373fea3490411
harsha-konda <konda.harsha1@gmail.com>
2019-01-12 10:01:01 -0800

    merge sort

--- src/main/scala/sort/MergeSort.scala
+++ src/main/scala/sort/MergeSort.scala
@@ -2,32 +2,24 @@ package sort
 
 object MergeSort extends App {
   def combine(a: List[Int], b: List[Int]): List[Int] = {
-    def combine(a: List[Int], b: List[Int], accum: List[Int]): List[Int] = {
-      a match {
-        case List() => accum ++ b
-        case head :: tail => {
-          if (b.isEmpty)
-            accum ++ a
-          else if (head > b.head) {
-            combine(tail, b, accum ++ List(head))
-          }
-          else
-            combine(a, b.tail, accum ++ List(b.head))
-        }
+    (a, b) match {
+      case (_, List()) => a
+      case (List(), _) => b
+      case (aHead :: aTail, bHead :: bTail) => {
+        if (aHead < bHead) aHead :: combine(aTail, b)
+        else bHead :: combine(a, bTail)
       }
     }
-    combine(a, b, List())
   }
 
-  def sort(l: List[Int]): List[Int] = l match {
-    case List() => l
-    case head :: List() => l
-    case a :: b :: List() => if (a > b) l else b :: a :: List()
-    case default => {
-      val middle = l.size / 2
-      val a = l.splitAt(middle)
-      combine(sort(a._1), sort(a._2))
+  def sort(l: List[Int]): List[Int] = {
+    val middle = l.size / 2
+    if (middle == 0) l
+    else {
+      val (left, right) = l.splitAt(middle)
+      combine(sort(left), sort(right))
     }
+
   }
 
 }
